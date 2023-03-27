f1f94c443200bf4bcb9d3bb656406e6bca0bb2e6
Mahmoud <mellebda@student.eelu.edu.eg>
2023-03-27 14:40:43 +0200

    refactor navigation in our app

---
+++ lib/core/utils/app_router.dart
@@ -0,0 +1,19 @@
+import 'package:go_router/go_router.dart';
+import 'package:untitled3/features/home/presentation/views/home_view.dart';
+import 'package:untitled3/features/splach/presentation/views/splach_view.dart';
+
+abstract class AppRouter{
+  static const String homepage = "/homeView";
+ static final router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => SplachView(),
+    ),
+    GoRoute(
+      path: homepage,
+      builder: (context, state) => HomeView(),
+    ),
+  ],
+);
+}
