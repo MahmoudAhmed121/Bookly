5c01b43ab8809758368838379c17ea87caa8ff05
Mahmoud <mellebda@student.eelu.edu.eg>
2023-03-28 15:43:07 +0200

    create similar books list view

--- lib/features/home/presentation/views/widget/book_details_view_body.dart
+++ lib/features/home/presentation/views/widget/book_details_view_body.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:untitled3/features/home/presentation/views/widget/best_seller_list_view_item.dart';
 
 import 'package:untitled3/features/home/presentation/views/widget/book_rating.dart';
 import 'package:untitled3/features/home/presentation/views/widget/books_details_action.dart';
 
 import 'package:untitled3/features/home/presentation/views/widget/custom_book_details_app_bar.dart';
 import 'package:untitled3/features/home/presentation/views/widget/custom_book_container_.dart';
+import 'package:untitled3/features/home/presentation/views/widget/similar_books_list_view.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
@@ -17,7 +19,9 @@ class BookDetailsViewBody extends StatelessWidget {
       ),
       child: Column(
         children: [
+          SizedBox(height: 25,),
           CustomAppBarBookDetails(),
+          SizedBox(height: 32,),
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 80),
             child: CustomBookContainer(),
@@ -52,8 +56,23 @@ class BookDetailsViewBody extends StatelessWidget {
             height: 37,
           ),
           BooksAction(),
+          SizedBox(
+            height: 50,
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              "You can also like",
+              style:
+                  TextStyle(fontWeight: FontWeight.w600, color: Colors.white),
+            ),
+          ),
+          SizedBox(height: 16,),
+          SimilarBooksListView(),
         ],
       ),
     );
   }
 }
+
+
--- lib/features/home/presentation/views/widget/similar_books_list_view.dart
+++ lib/features/home/presentation/views/widget/similar_books_list_view.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:untitled3/features/home/presentation/views/widget/custom_book_container_.dart';
 
-class FeatureBooksListView extends StatelessWidget {
-  const FeatureBooksListView({super.key});
+class SimilarBooksListView extends StatelessWidget {
+  const SimilarBooksListView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-     height: MediaQuery.of(context).size.height *.3,
+     height: MediaQuery.of(context).size.height *.17,
       child: ListView.builder(
         physics: BouncingScrollPhysics(),
         scrollDirection: Axis.horizontal,
