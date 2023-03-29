365c8253f58da081fb723f424edfacf68e0794c0
Mahmoud <mellebda@student.eelu.edu.eg>
2023-03-29 21:09:11 +0200

    create featured books method

--- lib/core/utils/errors/failure.dart
+++ lib/core/utils/errors/failure.dart
@@ -1,13 +1,10 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 abstract class Failure {
-
-   String errorMessage;
+  String errorMessage;
   Failure({
     required this.errorMessage,
   });
 }
 
-class ServerFailure extends Failure{
+class ServerFailure extends Failure {
   ServerFailure({required super.errorMessage});
-  
 }
