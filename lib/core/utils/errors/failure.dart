bfc9fb9cf84a1c81df4f09f0e2ae5c1390f1f031
Mahmoud <mellebda@student.eelu.edu.eg>
2023-03-29 20:38:39 +0200

    create featured books cubit state

--- lib/core/utils/errors/failure.dart
+++ lib/core/utils/errors/failure.dart
@@ -1,3 +1,13 @@
-abstract class Failure{
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+abstract class Failure {
+
+   String errorMessage;
+  Failure({
+    required this.errorMessage,
+  });
 }
 
+class ServerFailure extends Failure{
+  ServerFailure({required super.errorMessage});
+  
+}
--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -1,22 +1,55 @@
+import 'dart:math';
+
 import 'package:dio/dio.dart';
 import 'package:untitled3/features/home/data/models/Book%20model/book_model.dart';
 import 'package:untitled3/core/utils/errors/failure.dart';
 import 'package:dartz/dartz.dart';
 import 'package:untitled3/features/home/data/repos/home_repo.dart';
 
-class HomeRepoImpl implements HomeRepo{
+class HomeRepoImpl implements HomeRepo {
   @override
-  Future<Either<Failure, List<BookModel>>> fetchNewsBooks() {
-   final response =Dio().get("https://www.googleapis.com/books/v1/volumes?q=subject:Kids&Filtering=free-ebooks");
+  Future<Either<Failure, List<BookModel>>> fetchNewsBooks() async {
+    try {
+      final response = await Dio().get(
+          "https://www.googleapis.com/books/v1/volumes?q=subject:Kids&Filtering=free-ebooks");
+      List<BookModel> books = [];
 
-
-    throw UnimplementedError();
+      for (var iteam in response.data["items"]) {
+        books.add(BookModel.fromJson(iteam));
+      }
+      return right(books);
+    } on DioError catch (e) {
+      if (e.response == 400) {
+        return left(ServerFailure(errorMessage: "${e.message}"));
+      } else if (e.response == 401) {
+        return left(ServerFailure(errorMessage: "${e.message}"));
+      } else if (e.response == 404) {
+        return left(ServerFailure(errorMessage: "${e.message}"));
+      }
+    }
+    return left(ServerFailure(errorMessage: e.toString()));
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-  
-    throw UnimplementedError();
-  }
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async {
+    try {
+      final response = await Dio().get(
+          "https://www.googleapis.com/books/v1/volumes?q=subject:Kids&Filtering=free-ebooks");
+      List<BookModel> books = [];
 
+      for (var iteam in response.data["items"]) {
+        books.add(BookModel.fromJson(iteam));
+      }
+      return right(books);
+    } on DioError catch (e) {
+      if (e.response == 400) {
+        return left(ServerFailure(errorMessage: "${e.message}"));
+      } else if (e.response == 401) {
+        return left(ServerFailure(errorMessage: "${e.message}"));
+      } else if (e.response == 404) {
+        return left(ServerFailure(errorMessage: "${e.message}"));
+      }
+    }
+    return left(ServerFailure(errorMessage: e.toString()));
+  }
 }
