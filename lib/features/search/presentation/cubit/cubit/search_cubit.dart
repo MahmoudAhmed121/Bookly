3b3a3a9c007eca7cdf65885791cacc91e9de8359
Mahmoud <mellebda@student.eelu.edu.eg>
2023-04-07 21:23:19 +0200

    const done

--- lib/features/search/presentation/cubit/cubit/search_cubit.dart
+++ lib/features/search/presentation/cubit/cubit/search_cubit.dart
@@ -1,19 +1,17 @@
 
 
-import 'package:dartz/dartz.dart';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
  import 'package:equatable/equatable.dart';
- import 'package:flutter/cupertino.dart';
+
  import 'package:flutter/material.dart';
- import 'package:flutter_bloc/flutter_bloc.dart';
+
 
  import 'package:untitled3/features/home/data/models/book_model/book_model.dart';
 import 'package:untitled3/features/search/presentation/data/repo/search_repo.dart';
- import 'package:untitled3/features/search/presentation/data/repo/search_repo_impl.dart';
 
-import '../../../../../core/utils/errors/failure.dart';
 
  part 'search_state.dart';
 
